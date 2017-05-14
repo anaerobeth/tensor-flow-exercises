a296d1abd8175fceb0bf9676dec411cd3d72f4c8
Beth Tenorio <betsky98@gmail.com>
2017-05-13 23:23:03 -0400

    handle two tasks with a shared layer

--- multi-task.py
+++ multi-task.py
@@ -7,22 +7,20 @@ import numpy as np
 input_dimension = [10, 10]
 output_dimension = [10, 1]
 X = tf.placeholder("float", input_dimension, name="X")
-Y = tf.placeholder("float", output_dimension, name="Y")
+Y1 = tf.placeholder("float", output_dimension, name="Y1")
+Y2 = tf.placeholder("float", output_dimension, name="Y2")
 
-# Create Trainable Variable
-initial_W = np.zeros((10, 1))
-W = tf.Variable(initial_W, name="W", dtype="float32")
+# Define the weights for the layers
+shared_layer_weights = tf.Variable([10, 20], name="share_W")
+Y1_layer_weights = tf.Variable([20, 1], name="share_Y1")
+Y2_layer_weights = tf.Variable([20], name="share_Y2")
 
-# Define Loss Function
-Loss = tf.pow(tf.add(Y, -tf.matmul(X,W)), 2, name="Loss")
+# Construct the layers with RELU Activations
+shared_layer = tf.nn.relu(tf.matmul(X, shared_layer_weights))
+Y1_layer = tf.nn.relu(tf.matmul(shared_layer, Y1_layer_weights))
+Y2_shared_layer = tf.nn.relu(tf.matmul(shared_layer, Y2_layer_weights))
 
-# Set up the session
-with tf.Session() as sess:
-    sess.run(tf.initialize_all_variables())
-    Model_Loss = sess.run(
-        Loss,
-        {
-            X: np.random.rand(10, 10),
-            Y: np.random.rand(10).reshape(-1, 1)
-        })
-    print(Model_Loss)
+# Calculate Loss
+Y1_Loss = tf.nn.l2_Loss(Y1, Y1_layer)
+Y2_Loss = tf.nn.l2_Loss(Y2, Y2_layer)
+
